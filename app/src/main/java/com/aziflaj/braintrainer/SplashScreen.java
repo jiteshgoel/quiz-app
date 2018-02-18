75e043aaceb22d9e8bf72450f7cfb5e61977ec9b
academy-codex <academycodex@gmail.com>
2018-02-18 20:24:11 +0530

    qa app

---
+++ app/src/main/java/com/aziflaj/braintrainer/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.aziflaj.braintrainer;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }.start();
+    }
+}
